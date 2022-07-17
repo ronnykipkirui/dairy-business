9c57b83b86f160e07f7c3e63b3733ecf68263ab0
Ronny <ronnybet41@gmail.com>
2022-07-17 18:30:00 +0300

    add yearly functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,74 @@
-//alert("welcome to dairy business website")
-let costOfMilk= 45
-const shades =["Shade A", "Shade B", "Shade C", "Shade D"]
 
+function dailyProduction(id1,id2,id3,id4) {
+   
+    var litres1 = parseInt(document.getElementById(id1).value)
+    console.log("the production for "+id1+" is" + litres1 +"per day")
 
+    var litres2 = parseInt(document.getElementById(id2).value)
+    console.log("the production for "+id2+" is" + litres2 +"per day")
 
+    var litres3 = parseInt(document.getElementById(id3).value)
+    console.log("the production for "+id3+" is" + litres3 +"per day")
 
+    var litres4 = parseInt(document.getElementById(id4).value)
+    console.log("the production for "+id4+" is" + litres4 +"per day")
 
-function totalProduction(milk) {
-    let sumOfMilk = 0
+    var total = litres1+litres2+litres3+litres4;
 
-    for (i = 0; i < milk.lenght; i++) {
-        sumOfMilk += milk[i];
-        if (i == 0) {
-            console.log("the production for A is" + milk[0] * "per day")
-        }
-        else if (i == 1) {
-            console.log("the production for B is" + milk[1] * "per day")
-        }
-        else if (i == 2) {
-            console.log("the production for C is" + milk[2] * "per day")
-        }
-        else if (i == 3) {
-            console.log("the production for D is" + milk[3] * "per day")
-        }
-        else if (i == 4) {
-            console.log("the production for E is" + milk[4] * "per day")
-        }
-        else  {
-            console.log("the production for F is" + milk[5] * "per day")
-        }
-    }
-    console.log("the production for the day is "+ sumOfMilk* "litres of milk per day")
+    console.log("Total production is "+total+" litres per day")
+
+    
 }
-totalProduction([1])
+
+function weeklyProduction(id1,id2,id3,id4) {
+    var price = parseInt(document.getElementById('price').value)
+
+    var litres1 = parseInt(document.getElementById(id1).value)
+    console.log("the production for "+id1+" is" + litres1*7 +"per week")
+
+    var litres2 = parseInt(document.getElementById(id2).value)
+    console.log("the production for "+id2+" is" + litres2*7 +"per week")
+
+    var litres3 = parseInt(document.getElementById(id3).value)
+    console.log("the production for "+id3+" is" + litres3*7 +"per day")
+
+    var litres4 = parseInt(document.getElementById(id4).value)
+    console.log("the production for "+id4+" is" + litres4*7 +"per day")
+   
+    var total = litres1+litres2+litres3+litres4;
+
+    console.log("Total production is "+total*7+" litres per week")
+
+    console.log("total cost is Shs. "+total*7*price+" per week ")
+}
+
+function leap_year(id1,id2,id3,id4) {
+    var price = parseInt(document.getElementById('price').value)
+
+    var litres1 = parseInt(document.getElementById(id1).value)
+    console.log("the production for "+id1+" is" + litres1*7 +"per week")
+
+    var litres2 = parseInt(document.getElementById(id2).value)
+    console.log("the production for "+id2+" is" + litres2*7 +"per week")
+
+    var litres3 = parseInt(document.getElementById(id3).value)
+    console.log("the production for "+id3+" is" + litres3*7 +"per day")
+
+    var litres4 = parseInt(document.getElementById(id4).value)
+    console.log("the production for "+id4+" is" + litres4*7 +"per day")
 
 
-//production of milk in litres per shade each day
+    var total = litres1+litres2+litres3+litres4;
+
+    const monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+    for (var i = 0; i <= 11; i++) {
+        days =  new Date(2020,i+1,0).getDate();  // get number of days in a month
+        var sum = total*days
+
+        console.log("there is "+days+" days in "+monthNames[i]+" and production is "+sum+" cost is" +sum*price)
+
+    }
 
 
-
-
-
-
-//income per week for 45/- per litre
-
-
-
-
-//income per year 
-
-
-
-
-//laep year report
+}
